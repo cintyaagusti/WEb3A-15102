c6e7a756e826fd39e5af4fb30311b009efd49fb6
Cintya Agusti Anindita <cintyaagusti99@mail.ugm.ac.id>
2020-03-30 06:08:11 +0700

    Updates

---
+++ precache-manifest.eb08c934b1866cdd6e9e0e47872f672e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b828434c50c23d01dabcdab3adb9d35",
+    "url": "/WEB3A-15102-CINTYA-REACT/index.html"
+  },
+  {
+    "revision": "d261ec796b59431172a7",
+    "url": "/WEB3A-15102-CINTYA-REACT/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "8c9eb2773da4ea61c2ef",
+    "url": "/WEB3A-15102-CINTYA-REACT/static/css/main.34b6580d.chunk.css"
+  },
+  {
+    "revision": "d261ec796b59431172a7",
+    "url": "/WEB3A-15102-CINTYA-REACT/static/js/2.49dd5a23.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/WEB3A-15102-CINTYA-REACT/static/js/2.49dd5a23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c9eb2773da4ea61c2ef",
+    "url": "/WEB3A-15102-CINTYA-REACT/static/js/main.73033087.chunk.js"
+  },
+  {
+    "revision": "387dabef651bef574893",
+    "url": "/WEB3A-15102-CINTYA-REACT/static/js/runtime-main.68b8fc78.js"
+  }
+]);
